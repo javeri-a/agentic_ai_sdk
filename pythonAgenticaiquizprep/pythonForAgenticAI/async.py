55d55a9be02f9646636381281d5fb0bcae283ec0
javeri-a <javeriaqureshi746@gmail.com>
2025-07-06 23:47:25 +0500

    add new func

--- pythonAgenticaiquizprep/pythonForAgenticAI/async.py
+++ pythonAgenticaiquizprep/pythonForAgenticAI/async.py
@@ -30,3 +30,29 @@ async def main():
     )
 
 asyncio.run(main())
+
+async def paratha_banao():
+    print("🍳 Paratha ban raha hai...")
+    await asyncio.sleep(2)   # 2 second rukenge
+    print("✅ Paratha tayyar!")
+
+async def main():
+    await asyncio.gather(
+        chai_banao(),
+        paratha_banao()
+    )
+
+asyncio.run(main())
+
+async def paratha_banao():
+    print("🍳 Paratha ban raha hai...")
+    await asyncio.sleep(2)   # 2 second rukenge
+    print("✅ Paratha tayyar!")
+
+async def main():
+    await asyncio.gather(
+        chai_banao(),
+        paratha_banao()
+    )
+
+asyncio.run(main())
