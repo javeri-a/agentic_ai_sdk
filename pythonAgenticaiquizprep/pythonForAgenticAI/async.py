c8210f5c4588c76c6dad322221845108a960e525
javeri-a <javeriaqureshi746@gmail.com>
2025-07-11 08:59:12 +0500

    add new function

--- pythonAgenticaiquizprep/pythonForAgenticAI/async.py
+++ pythonAgenticaiquizprep/pythonForAgenticAI/async.py
@@ -31,10 +31,7 @@ async def main():
 
 asyncio.run(main())
 
-async def paratha_banao():
-    print("🍳 Paratha ban raha hai...")
-    await asyncio.sleep(2)   # 2 second rukenge
-    print("✅ Paratha tayyar!")
+
 
 async def main():
     await asyncio.gather(
@@ -44,54 +41,12 @@ async def main():
 
 asyncio.run(main())
 
-async def paratha_banao():
-    print("🍳 Paratha ban raha hai...")
-    await asyncio.sleep(2)   # 2 second rukenge
-    print("✅ Paratha tayyar!")
 
-async def main():
-    await asyncio.gather(
-        chai_banao(),
-        paratha_banao()
-    )
+def is_even(number):
+    if number % 2 == 0:
+        return True
+    else:
+        return False
 
-asyncio.run(main())
-
-async def paratha_banao():
-    print("🍳 Paratha ban raha hai...")
-    await asyncio.sleep(2)   # 2 second rukenge
-    print("✅ Paratha tayyar!")
-
-async def main():
-    await asyncio.gather(
-        chai_banao(),
-        paratha_banao()
-    )
-
-asyncio.run(main())
-
-async def paratha_banao():
-    print("🍳 Paratha ban raha hai...")
-    await asyncio.sleep(2)   # 2 second rukenge
-    print("✅ Paratha tayyar!")
-
-async def main():
-    await asyncio.gather(
-        chai_banao(),
-        paratha_banao()
-    )
-
-asyncio.run(main())
-
-async def paratha_banao():
-    print("🍳 Paratha ban raha hai...")
-    await asyncio.sleep(2)   # 2 second rukenge
-    print("✅ Paratha tayyar!")
-
-async def main():
-    await asyncio.gather(
-        chai_banao(),
-        paratha_banao()
-    )
-
-asyncio.run(main())
+print(is_even(4))  # Output: True
+print(is_even(7))
