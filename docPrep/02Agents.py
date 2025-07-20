b0134f5945b5bb1d44f82c090ee2828e5036cf7e
javeri-a <javeriaqureshi746@gmail.com>
2025-07-20 21:43:25 +0500

    add handsoff fun

--- docPrep/02Agents.py
+++ docPrep/02Agents.py
@@ -1,3 +1,7 @@
+#_____________________________________________________________
+#                   Function Tool
+#_____________________________________________________________
+
 from agents import Agent, model_settings , function_tool
 
 @function_tool
@@ -23,4 +27,28 @@ agent  = Agent(
     instructions="You are an agent that provides cooking recipes.",
     tools=[chef],
     model="03-mini"
+)
+
+
+#_____________________________________________________________
+#                   Handsoff
+#_____________________________________________________________
+
+booking_agent = Agent(
+    name="Booking agent",
+    instructions="You are an agent that handles booking requests."
+)
+refund_agent = Agent(
+    name="Refund agent",
+    instructions="You are an agent that handles refund requests."
+)
+
+triage_agent = Agent(
+    name="Triage agent",
+    instructions=(
+        "Help the user with their questions."
+        "If they ask about booking, handoff to the booking agent."
+        "If they ask about refunds, handoff to the refund agent."
+    ),
+    handoffs=[booking_agent, refund_agent],
 )
